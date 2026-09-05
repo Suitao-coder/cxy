afe609f57cd12bc1d85a51dbde22afcf15c58670
suitao <suitao_2022@qq.com>
2026-09-05 21:14:08 +0800

    Add files via upload

---
+++ birthday-gift/data.js
@@ -0,0 +1,24 @@
+window.DATA = {
+  "title": "与你有关的日子",
+  "subtitle": "那些值得被记住的，每一天都在长大。",
+  "entries": [
+    {
+      "name": "第一次见面",
+      "date": "2026-05-01",
+      "note": "那天你穿了一件很好看的白裙子。",
+      "annual": false
+    },
+    {
+      "name": "在一起",
+      "date": "2026-06-06",
+      "note": "",
+      "annual": false
+    },
+    {
+      "name": "你的生日",
+      "date": "2008-03-14",
+      "note": "一年里最特别的一天。",
+      "annual": true
+    }
+  ]
+};
